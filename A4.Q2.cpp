7b13f375397676cdd453606d8940577119ac9544
Dhanush T <120152745+DHANUSHZZ@users.noreply.github.com>
2023-04-18 08:23:30 +0530

    Update A4.Q2.cpp

--- A4.Q2.cpp
+++ A4.Q2.cpp
@@ -1,33 +1,28 @@
-#include<stdio.h>
-struct employee
-{
-     int e;
-     char name[20];
-     char designation[20];
-     char dept[20];
-     int sal;
-}
-int main()
-{
-     struct employee a;
-     printf("Enter Employee Details:\n");
-     printf("-------------------------------\n");
-     printf("Enter Employee-Id : ");
-     scanf("%d",&a.e);
-     printf("Enter Name        : ");
-     scanf("%s",a.name);
-     printf("Enter Designation : ");
-     scanf("%s",a.designation);
-     printf("Enter Department  : ");
-     scanf("%s",a.dept);
-     printf("Enter Salary      : ");
-     scanf("%d",&a.sal);
-     printf("-------------------------------");
-     printf("\nEmployee Details: \n---------------------------------\n");
-     printf("Employee-Id : %d\n",a.e);
-     printf("Name        : %s\n",a.name);
-     printf("Designation : %s\n",a.designation);
-     printf("Department  : %s\n",a.dept);
-     printf("Salary      : %d\n",a.sal);
-     return 0;
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_PLAYERS 11
+
+struct player {
+    char name[50];
+    int runs_scored;
+};
+int main() {
+    struct player team[MAX_PLAYERS];
+    int num_players, total_runs = 0;
+    printf("Enter the number of players: ");
+    scanf("%d", &num_players);
+    for (int i = 0; i < num_players; i++) {
+        printf("Enter the name of player %d: ", i+1);
+        scanf("%s", team[i].name);
+        printf("Enter the runs scored by %s: ", team[i].name);
+        scanf("%d", &team[i].runs_scored);
+        total_runs += team[i].runs_scored;
+    }
+    printf("\nBatting Information:\n");
+    for (int i = 0; i < num_players; i++) {
+        printf("%s\t\t%d\n", team[i].name, team[i].runs_scored);
+    }
+    printf("Total runs scored: %d\n", total_runs);
+    return 0;
 }
